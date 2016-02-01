bf063fe1a73c47670e61b2ab72b435d6a9bf7002
inkaxd <michalczak.ka@gmail.com>
2016-02-01 23:53:04 +0100

    Tu juz przeszlo i jest OK

--- src/Projekt/WypozyczalniaSamochodow/Entity/Review.php
+++ src/Projekt/WypozyczalniaSamochodow/Entity/Review.php
@@ -62,3 +62,153 @@ public function setReview($review) {
 	
 
 
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set carID
+     *
+     * @param integer $carID
+     * @return Review
+     */
+    public function setCarID($carID)
+    {
+        $this->carID = $carID;
+
+        return $this;
+    }
+
+    /**
+     * Get carID
+     *
+     * @return integer 
+     */
+    public function getCarID()
+    {
+        return $this->carID;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Review
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     * @return Review
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Review
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     * @return Review
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+	//email: kulpsychologia2014@gmail.com
+	//hasło: psychologiakul2014
+
+    /**
+     * Set active
+     *
+     * @param integer $active
+     * @return Review
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return integer 
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
