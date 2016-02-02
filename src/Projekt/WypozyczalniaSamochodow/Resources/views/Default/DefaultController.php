e9e0b48f08c4fd7a9646c9fbe95135d058bc187e
panimarkowska <panimarkowska@gmail.com>
2016-02-02 17:15:31 +0100

    zmiany cd defaultcontroller

--- src/Projekt/WypozyczalniaSamochodow/Resources/views/Default/DefaultController.php
+++ src/Projekt/WypozyczalniaSamochodow/Resources/views/Default/DefaultController.php
@@ -109,3 +109,127 @@ class DefaultController extends Controller
 		));
     }
 	
+	
+	
+	/**
+     * @Route("/car/{$id}", name="_car")
+     * @Template()
+     */
+	public function carAction($id, Request $Request)
+	{
+		
+	
+	$car = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Car')
+        ->findOneById($id);
+		
+	$review = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Review')
+        ->findBy(array('carID' => $id));
+	
+	return $this->render('ProjektWypozyczalniaSamochodow:Default:Car.html.twig', array(
+	'car' => $car,
+	'review' => $review,
+	//'form' => $form->createView(),
+		));
+	}
+	
+	
+	/**
+     * @Route("/addToCart", name="addToCart")
+     * @Template()
+     */
+    public function addToCartAction($id, Request $Request)
+    {
+        
+		$car = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Car')
+        ->find($id);
+		
+		
+		$cart = new Cart();
+		
+		$cart -> setUserID();
+		$cart -> setCarID($car->getId());
+		
+		return array();
+    }
+
+    public function historyAction()
+    {
+	
+		$user = $this->getUser();
+			if (!is_object($user) || !$user instanceof UserInterface) {
+				throw new AccessDeniedException('Zaloguj się!');
+			}
+		
+		
+		$history = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Cart')
+        ->findBy(array('userID' => $user->getId(), 'OK' => 1));
+	
+	
+        return $this->render('ProjektWypozyczalniaSamochodow:Default:history.html.twig', array(
+			'history' => $history,
+		));
+    }
+	
+	
+	
+	/**
+     * @Route("/allCars", name="allCars")
+     * @Template()
+     */
+    public function allCarsAction()
+    {
+	
+		 $cars = $this->getDoctrine()
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Car')
+        ->findAll();
+	
+	
+        return $this->render('ProjektWypozyczalniaSamochodow:Default:allCars.html.twig', array(
+			'cars' => $cars,
+		));
+    }
+	
+	
+	/**
+     * @Route("/cart", name="cart")
+     * @Template()
+     */
+    public function cartAction(Request $request)
+    {
+	//trzeba tu napisać odczyt z ciasteczka
+	
+	$user = $this->getUser();
+			if (!is_object($user) || !$user instanceof UserInterface) {
+				throw new AccessDeniedException('Zaloguj się!');
+			}
+	
+	
+	$cart = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Cart')
+        ->findBy(array('userID' => $user->getId(), 'OK' => 0));
+	
+	
+	$cars = array();
+	
+	foreach ($cart as $c) {
+		$cars[] = $this->getDoctrine() 
+        ->getRepository('Projekt\WypozyczalniaSamochodow\Entity\Car')
+        ->findOneById($c->getCarID());
+	}
+	
+	
+        return $this->render('ProjektWypozyczalniaSamochodow:Default:cart.html.twig', array(
+		'cart' => $cart,
+		'cars' => $cars,
+		//'cost' => $cost,
+		//'ID' => $ID,
+		));
+		
+		
+    }
+	
+}
