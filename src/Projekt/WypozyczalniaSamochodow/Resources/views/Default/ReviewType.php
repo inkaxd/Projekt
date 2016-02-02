39e441e672222bb3648d38fb3d88e129f063d961
panimarkowska <panimarkowska@gmail.com>
2016-02-02 17:37:15 +0100

    modyfikacja reviewtype

--- src/Projekt/WypozyczalniaSamochodow/Resources/views/Default/ReviewType.php
+++ src/Projekt/WypozyczalniaSamochodow/Resources/views/Default/ReviewType.php
@@ -7,3 +7,30 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 }
+
+class ReviewType extends AbstractType{
+
+ public function getName() {
+       return 'review';
+   }
+
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+				//->add('contact','text',array('mapped'=>false))
+                ->add('title', 'text', array(
+                    'label' => 'Tytuł'
+                ))				
+				->add('description', 'textarea', array(
+                    'label' => 'Recenzja'
+        //        ))
+       //         ->add('save', 'submit', array(
+         //           'label' => 'Zapisz recencję'
+                ));
+    }
+	
+	  public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Projekt\WypozyczalniaSamochodow\Entity\Review'
+        ));
+
+}
